90dec1f18aeb496af664cdcc34ac16806d1d1d04
HudsonDukette <hudsoduk@gmail.com>
2026-03-29 14:20:19 +0000

    liff

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@workspace/api-client-react',
+        replacement: path.resolve(__dirname, 'lib/api-client-react/src/index.ts')
+      }
+    ]
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+});
