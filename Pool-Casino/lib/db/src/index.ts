6c7331252f2f6682ca144db8f7d23602aaf66eab
HudsonDukette <hudsoduk@gmail.com>
2026-03-29 03:26:45 +0000

    Fix ES module imports for path in database configuration

--- Pool-Casino/lib/db/src/index.ts
+++ Pool-Casino/lib/db/src/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import path from "path";
+import * as path from "path";
 import * as schema from "./schema";
 
 const client = createClient({
